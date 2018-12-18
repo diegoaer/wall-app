895e483e7a4ac4b48197bb2d2c642ed0662cd1fc
diegoaer <diegoaer@live.com>
2018-12-17 21:36:50 -0600

    Fix - added queryset to UserViewSet

--- wall/users/views.py
+++ wall/users/views.py
@@ -1,10 +1,12 @@
 """Views for the users app"""
 from rest_framework.viewsets import GenericViewSet, mixins
 
+from django.contrib.auth.models import User
 from .serializers import UserSerializer
 
 
 # Create your views here.
 class UserViewSet(mixins.CreateModelMixin, GenericViewSet):
     """Enpoint for user creation"""
+    queryset = User.objects.none()
     serializer_class = UserSerializer
