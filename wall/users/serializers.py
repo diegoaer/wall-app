2eec0cee589a8a4bd8fc5db1425f71ddd1a28715
diegoaer <diegoaer@live.com>
2018-12-17 21:37:08 -0600

    Fix - typo in UserSerializer password field

--- wall/users/serializers.py
+++ wall/users/serializers.py
@@ -9,4 +9,4 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'pasword')
+        fields = ('username', 'email', 'password')
