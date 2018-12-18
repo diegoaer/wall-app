f31638fd4f95f6195da03cd6b0dd54b3189d867c
diegoaer <diegoaer@live.com>
2018-12-17 20:01:19 -0600

    Added comments to posts app models

--- wall/posts/models.py
+++ wall/posts/models.py
@@ -1,8 +1,10 @@
+"""Post module models"""
 from django.db import models
 from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Post(models.Model):
+    """A wall post"""
     content = models.TextField()
     user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
