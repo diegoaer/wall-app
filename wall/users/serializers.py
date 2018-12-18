9db7b7035b01952f414ec534ec802c19957d65e3
diegoaer <diegoaer@live.com>
2018-12-18 13:15:04 -0600

    Fix - Added user password validation in UserSerializer, user app

--- wall/users/serializers.py
+++ wall/users/serializers.py
@@ -1,4 +1,5 @@
 """Serializers for users app"""
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 
 from django.contrib.auth.models import User
@@ -10,6 +11,20 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
+
+    def validate(self, attrs):
+        """
+        Validates the password again with django validators, usefull for validators that do not 
+        depend only on the password
+        """
+        dummy_user = User(**attrs)
+        validate_password(attrs['password'], user=dummy_user)
+        return attrs
+
+    def validate_password(self, value):
+        """Validates the password with django validators"""
+        validate_password(value)
+        return value
 
     def create(self, validated_data):
         instance = super(UserSerializer, self).create(validated_data)
