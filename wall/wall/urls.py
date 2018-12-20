063985deabe09779d395f2f476ddce178d09f4ce
diegoaer <diegoaer@live.com>
2018-12-20 12:44:39 -0600

    Fix - correct token auth path, added login to rest framework browsable API

--- wall/wall/urls.py
+++ wall/wall/urls.py
@@ -1,5 +1,5 @@
 """wall URL Configuration"""
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views as token_views
 
@@ -12,5 +12,6 @@ ROUTER.register(r'users', user_views.UserViewSet)
 
 urlpatterns = [
     path('', include(ROUTER.urls)),
-    path(r'^auth/', token_views.obtain_auth_token),
+    re_path(r'^auth/', token_views.obtain_auth_token, name='token-auth'),
+    path('api-auth/', include('rest_framework.urls')),
 ]
