df7fdf9edf7cfd1e2a19ba2dc7a05e9a523ff2f9
diegoaer <diegoaer@live.com>
2018-12-20 13:16:04 -0600

    Added send email on user creation capability in UserViewSet in users app

--- wall/users/views.py
+++ wall/users/views.py
@@ -1,7 +1,8 @@
 """Views for the users app"""
 from rest_framework.viewsets import GenericViewSet, mixins
-
+from django.core.mail import send_mail
 from django.contrib.auth.models import User
+
 from .serializers import UserSerializer
 
 
@@ -10,3 +11,13 @@ class UserViewSet(mixins.CreateModelMixin, GenericViewSet):
     """Enpoint for user creation"""
     queryset = User.objects.none()
     serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        """Overrides the defualt ViewSet create to send an email on user creation"""
+        super(UserViewSet, self).perform_create(serializer)
+        send_mail(
+            'Welcome',
+            'This is  welcome email that would be way better in a real app!',
+            'contact@wallapp.com', [serializer.instance.email],
+            fail_silently=False
+        )
