03124bc762ad4cdea4d86f20378e80ca822852fd
diegoaer <diegoaer@live.com>
2018-12-20 11:26:43 -0600

    Added token auth endpoint

--- wall/wall/urls.py
+++ wall/wall/urls.py
@@ -1,6 +1,7 @@
 """wall URL Configuration"""
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views as token_views
 
 from posts import views as post_views
 from users import views as user_views
@@ -11,4 +12,5 @@ ROUTER.register(r'users', user_views.UserViewSet)
 
 urlpatterns = [
     path('', include(ROUTER.urls)),
+    path(r'^auth/', token_views.obtain_auth_token),
 ]
