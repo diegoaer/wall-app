e49a58bd94dd0cd734e770ca0da282659a12070b
diegoaer <diegoaer@live.com>
2018-12-20 11:32:06 -0600

    Added authentication to posts viewset in posts app

--- wall/posts/views.py
+++ wall/posts/views.py
@@ -1,5 +1,6 @@
 """Views for the posts app"""
 from rest_framework.viewsets import GenericViewSet, mixins
+from rest_framework import permissions
 
 from .models import Post
 from .serializers import PostSerializer
@@ -10,3 +11,4 @@ class PostViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, GenericViewSet
     """Enpoints for creation and listing of Posts"""
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
