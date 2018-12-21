c6358f371eefff8f89dece9b06d5def93971f890
diegoaer <diegoaer@live.com>
2018-12-20 20:06:35 -0600

    SERVER Changed PostSerializer so that it returns the date and username instead of user id in the users app

--- wall/posts/serializers.py
+++ wall/posts/serializers.py
@@ -9,4 +9,11 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ('content', 'user')
+        fields = ('content', 'user', 'date')
+
+    def to_representation(self, instance):
+        """Returns the username instead of the user id"""
+        ret = super(PostSerializer, self).to_representation(instance)
+        if instance.user:
+            ret['user'] = instance.user.username
+        return ret
