1fe920d91b18d8d2510640b7830d1850d3f7ccfe
diegoaer <diegoaer@live.com>
2018-12-20 20:26:49 -0600

    APP Creted user class

---
+++ app/app/src/main/java/gt/com/diego/wallapp/content/User.java
@@ -0,0 +1,36 @@
+package gt.com.diego.wallapp.content;
+
+
+/** A user that can post */
+public class User {
+    private String username;
+    private String password;
+    private String email;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public User setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+}
